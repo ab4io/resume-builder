887a7d8390ec560f0580fb25cf218262ff7a1051
ab4io <ayalkbettesfahun@gmail.com>
2024-07-14 17:18:18 +0300

    adding-authentication

---
+++ src/home/index.jsx
@@ -0,0 +1,12 @@
+import { UserButton } from '@clerk/clerk-react'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Homepage
+      <UserButton />
+    </div>
+  )
+}
+
+export default Home
