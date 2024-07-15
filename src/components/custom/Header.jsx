914113b1fc276b51695f478e0509e2dbfe25470e
ab4io <ayalkbettesfahun@gmail.com>
2024-07-15 14:57:20 +0300

    Update

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,8 +1,32 @@
+import { UserButton, useUser } from '@clerk/clerk-react'
 import React from 'react'
+import { Button } from '../ui/button';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const { user, isSignedIn } = useUser();
+
   return (
-    <div>Header</div>
+    <div className='p-3 flex justify-between shadow-md'>
+      <img src="/logo.svg" width={100} height={100} alt="" />
+      
+      { isSignedIn ?
+        
+        <div className='flex items-center gap-2'>
+          <Link to={'/dashboard'}>
+            <Button variant="outline">Dashboard</Button>
+          </Link>
+          <UserButton />
+        </div> :
+        
+        <div className='flex items-center gap-2'>
+          <Link to={'auth/sign-in'}>
+            <Button>Get Started</Button>
+          </Link>
+          <UserButton />
+        </div>
+      }
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#d0bdf4",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
